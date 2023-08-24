7ca5f1d0e30828263df201e488e22e0c641a411c
Breno Amorim Barbosa <130618894+tkLr7@users.noreply.github.com>
2023-08-24 17:03:25 -0300

    Add files via upload

---
+++ Exercicio1/src/somarnumeros.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class somarnumeros {
+	public static Scanner scr= new Scanner(System.in);
+
+	
+	public static void main(String args[]){
+		//variaveis
+		int x, y, z; 
+		
+		//ler primeiro numero
+		System.out.println("Primeiro numero:");
+		x = scr.nextInt();
+		
+		System.out.println("Segundo numero:");
+		y = scr.nextInt();
+		
+		//soma
+		
+		z= x + y;
+		
+		//mostra soma
+		System.out.println("Soma:" + z);
+	}
+}
